f282e464b99fb9e1eaa399f03c942662663e7653
Harry Chew <60672261+harry-chew@users.noreply.github.com>
2022-12-01 18:40:22 +0000

    feat: car manager
    car manager script for changing components/colour

---
+++ Assets/CarManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+internal class CarManager : MonoBehaviour
+{
+    private static List<ICarComponent> carComponents = new();
+
+    [SerializeField] private List<ICarComponent> myCarComponents;
+    [SerializeField] private List<WheelRim> myWheelRims;
+
+    private static CarManager Instance;
+    public static CarManager GetInstance()
+    {
+        if(Instance != null)
+        {
+            return Instance;
+        }
+        else
+        {
+            Instance = FindObjectOfType<CarManager>();
+            if(Instance != null)
+            {
+                return Instance;
+            }
+            else
+            {
+                Instance = new CarManager();
+                return Instance;
+            }
+        }
+    }
+
+    private void Start()
+    {
+        Instance = this;
+
+        foreach(ICarComponent component in myWheelRims)
+        {
+            carComponents.Add(component);
+        }
+    }
+
+    public List<ICarComponent> GetCarComponents(ComponentType wheelRim)
+    {
+        return carComponents;
+    }
+}
